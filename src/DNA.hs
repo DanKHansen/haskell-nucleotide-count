b843cb0b0a9fa502317537c88f5a6e153bfdad4a
Dan Kjeldstrøm Hansen <dankh@live.dk>
2025-07-10 13:53:04 +0200

    refactor

--- src/DNA.hs
+++ src/DNA.hs
@@ -1,21 +1,18 @@
 module DNA (nucleotideCounts, Nucleotide (..)) where
 
-import Data.List (group, sort)
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.Maybe (fromJust)
 
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Show)
 
 nucleotideCounts :: String -> Either String (Map Nucleotide Int)
 nucleotideCounts xs
-  | all (`elem` "ACGT") xs = Right $ M.fromList $ map (\cs -> (fromJust $ toN $ head cs, length cs)) $ group $ sort xs
+  | all (`elem` "ACGT") xs = Right $ M.fromListWith (+) [(n, 1) | c <- xs, Just n <- [toN c]]
   | otherwise = Left "Error"
 
 toN :: Char -> Maybe Nucleotide
-toN c = case c of
-  'A' -> Just A
-  'C' -> Just C
-  'G' -> Just G
-  'T' -> Just T
-  _ -> Nothing
+toN 'A' = Just A
+toN 'C' = Just C
+toN 'G' = Just G
+toN 'T' = Just T
+toN _ = Nothing
